610014a76b264dd7fa9a6f1630bbaecca9bb271f
ducanhpham <fostter2@gmail.com>
2025-09-01 13:49:19 +0700

    Setup redis kafka elas

--- src/main/java/com/example/demotech/DemotechApplication.java
+++ src/main/java/com/example/demotech/DemotechApplication.java
@@ -2,8 +2,10 @@ package com.example.demotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DemotechApplication {
 
     public static void main(String[] args) {
